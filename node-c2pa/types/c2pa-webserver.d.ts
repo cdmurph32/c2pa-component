bfc3bbd157e66357d54ca6370d4ad7c92a01ef8c
Colin Murphy <colmurph@adobe.com>
2025-03-25 18:55:05 +0100

    fix

---
+++ node-c2pa/types/c2pa-webserver.d.ts
@@ -0,0 +1,11 @@
+// world adobe:cai/c2pa-webserver@0.1.0
+export type * as AdobeCaiC2pa010 from './interfaces/adobe-cai-c2pa.js'; // import adobe:cai/c2pa@0.1.0
+export type * as AdobeCaiTypes010 from './interfaces/adobe-cai-types.js'; // import adobe:cai/types@0.1.0
+export type * as WasiClocksMonotonicClock022 from './interfaces/wasi-clocks-monotonic-clock.js'; // import wasi:clocks/monotonic-clock@0.2.2
+export type * as WasiClocksWallClock022 from './interfaces/wasi-clocks-wall-clock.js'; // import wasi:clocks/wall-clock@0.2.2
+export type * as WasiFilesystemTypes022 from './interfaces/wasi-filesystem-types.js'; // import wasi:filesystem/types@0.2.2
+export type * as WasiHttpTypes022 from './interfaces/wasi-http-types.js'; // import wasi:http/types@0.2.2
+export type * as WasiIoError022 from './interfaces/wasi-io-error.js'; // import wasi:io/error@0.2.2
+export type * as WasiIoPoll022 from './interfaces/wasi-io-poll.js'; // import wasi:io/poll@0.2.2
+export type * as WasiIoStreams022 from './interfaces/wasi-io-streams.js'; // import wasi:io/streams@0.2.2
+export * as incomingHandler from './interfaces/wasi-http-incoming-handler.js'; // export wasi:http/incoming-handler@0.2.2
